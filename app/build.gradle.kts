b768f9fa563db6d19865871021798f5ead8b9304
Samuel Albareda Zumelzu <sazumelzu@etu.uqac.ca>
2023-11-30 19:07:40 -0500

    still not working hiltWorker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,49 +50,67 @@ android {
 
 dependencies {
 
+
+    val kotlinVersion = "1.8.10"
+    val composeVersion = "1.4.2"
+    val activityComposeVersion = "1.7.1"
+    val hiltVersion="2.45"
+    val navComposeVersion = "1.0.0"
+    val roomVersion = "2.5.1"
+    val workVersion = "2.7.1"
+    val hiltWorkVersion = "1.0.0"
+    val retrofitVersion = "2.9.0"
+
+
     implementation("androidx.wear.compose:compose-material:1.2.1")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("com.google.accompanist:accompanist-permissions:0.33.2-alpha")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("androidx.hilt:hilt-work:1.0.0")
 
 
     // Room implementation with KSP
-    val roomVersion = "2.6.0"
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.45")
-    ksp("com.google.dagger:hilt-android-compiler:2.45")
-    ksp("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:$navComposeVersion")
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    // Worker
+    implementation("androidx.work:work-runtime-ktx:$workVersion")
+    implementation("androidx.hilt:hilt-work:$hiltWorkVersion")
+    ksp("androidx.hilt:hilt-compiler:$hiltWorkVersion")
+
 
     // Retrofit (include okhttp)
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.core:core-ktx:$kotlinVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:$activityComposeVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.5")
-    implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation("androidx.window:window:1.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:$composeVersion")
+
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
